02cfd95803335e4c73d6102c21d7303616a1c9a5
TerenceNg03 <stoicism03@gmail.com>
2023-07-30 07:32:38 +0800

    Simple service

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Lib
+import Network.Socket (Family (AF_INET), PortNumber, SockAddr (SockAddrInet), SocketType (Stream), bind, listen, socket, accept, socketToHandle)
+import System.IO (BufferMode (LineBuffering), Handle, hGetLine, hPrint, hPutStrLn, hSetBuffering, IOMode (ReadWriteMode), hClose)
+import Fmt (format)
+import Control.Monad (forever)
+import Control.Concurrent (forkFinally)
+
+talk :: Handle -> IO ()
+talk h = do
+    hSetBuffering h LineBuffering
+    loop
+  where
+    loop = do
+        line <- hGetLine h
+        if line == "end"
+            then
+                hPutStrLn
+                    h
+                    ( "Thank you for using the "
+                        ++ "Haskell doubling service."
+                    )
+            else do
+                hPrint h (2 * (read line :: Integer))
+                loop
 
 main :: IO ()
-main = someFunc
+main = do
+    sock <- socket AF_INET Stream 0
+    bind sock (SockAddrInet port 0)
+    listen sock 5
+    putStrLn $ format "Listening to port {}" (show port)
+    forever $ do
+        (sock', addr) <- accept sock
+        putStrLn $ format "Accepted connection : {}" (show addr)
+        handle <- socketToHandle sock' ReadWriteMode 
+        forkFinally (talk handle) (const $ hClose handle)
+
+port :: PortNumber
+port = 44444
